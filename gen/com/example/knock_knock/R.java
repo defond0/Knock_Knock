9be357a9d8b0beb65b406f7bcf000914ea199e24
Jeff DeFond <jeffdefond@gmail.com>
2014-04-02 20:40:31 -0700

    Added Skeleton to SoundSelection Xml and TarsosDsp library is now in the build
    There is now a scroll view containing a table layout that we can add
    checkboxes too for our sounds. The notification settings button does
    nothing. The Add New Sound button goes to the training menu.

--- gen/com/example/knock_knock/R.java
+++ gen/com/example/knock_knock/R.java
@@ -25,13 +25,26 @@ public final class R {
         public static final int soundoff=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080013;
+        public static final int addNewSoundButton=0x7f08000d;
         public static final int backButton=0x7f080000;
-        public static final int messageText=0x7f080005;
-        public static final int onButton=0x7f080002;
-        public static final int soundSettingsButton=0x7f080003;
-        public static final int splashTitle=0x7f080004;
-        public static final int trainingButton=0x7f080001;
+        public static final int checkBox1=0x7f080005;
+        public static final int checkBox2=0x7f080007;
+        public static final int checkBox3=0x7f080009;
+        public static final int checkBox4=0x7f08000b;
+        public static final int messageText=0x7f080012;
+        public static final int notificationSettingsButton=0x7f08000c;
+        public static final int onButton=0x7f08000f;
+        public static final int soundSelectionScroller=0x7f080002;
+        public static final int soundSelectionTitle=0x7f080001;
+        public static final int soundSettingsButton=0x7f080010;
+        public static final int soundTable=0x7f080003;
+        public static final int splashTitle=0x7f080011;
+        public static final int tableRow1=0x7f080004;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f080008;
+        public static final int tableRow4=0x7f08000a;
+        public static final int trainingButton=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_sound_settings=0x7f030000;
@@ -46,6 +59,7 @@ public final class R {
     public static final class string {
         public static final int ON=0x7f050006;
         public static final int action_settings=0x7f050001;
+        public static final int addNewSound=0x7f050011;
         public static final int app_name=0x7f050000;
         public static final int backButton=0x7f05000f;
         public static final int colorBG=0x7f05000a;
@@ -55,6 +69,8 @@ public final class R {
         public static final int colorYellow=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int msgWelcome=0x7f05000e;
+        public static final int notificationSettings=0x7f050012;
+        public static final int soundSelectionTitle=0x7f050010;
         public static final int splashSoundSettings=0x7f050008;
         public static final int splashTraining=0x7f050007;
         public static final int title_activity_sound_settings=0x7f050004;
