f1b3499bfa2d377a10affd26b26182269294d752
sobbert <sethanderson@berkeley.edu>
2014-04-21 18:40:02 -0700

    Added support files for new images

--- gen/com/example/knock_knock/R.java
+++ gen/com/example/knock_knock/R.java
@@ -33,12 +33,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int audio_placeholder=0x7f020000;
-        public static final int ic_action_discard=0x7f020001;
-        public static final int ic_action_edit=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int kk_launcher=0x7f020004;
-        public static final int kkicon=0x7f020005;
-        public static final int soundoff=0x7f020006;
+        public static final int blue_kk_icon=0x7f020001;
+        public static final int ic_action_discard=0x7f020002;
+        public static final int ic_action_edit=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int kk_launcher=0x7f020005;
+        public static final int kkicon=0x7f020006;
+        public static final int kklogo=0x7f020007;
+        public static final int soundoff=0x7f020008;
     }
     public static final class id {
         public static final int action_bar_home=0x7f09001b;
